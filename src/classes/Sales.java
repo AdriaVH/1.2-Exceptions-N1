150f140de96de3384bf515247bc14387d7d7a08d
adriaVH <adriavargashernandez@gmail.com>
2025-02-19 13:34:36 +0100

    class and exception creation

---
+++ src/classes/Sales.java
@@ -0,0 +1,11 @@
+package src.classes;
+
+public class Sales extends IndexOutOfBoundsException {
+private double totalPrice;
+private Product[] products;
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
